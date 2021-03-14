3938937adc8e19ca4ec34cd416af54820ac4a531
Fvlly <luki0000001@gmail.com>
2021-03-14 19:13:44 +0100

    added hamburger

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector(".toggle-menu");
+const navItems = document.querySelector(".navbar-items");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+});
